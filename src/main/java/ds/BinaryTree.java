b1a40a7329bc00ee6fe9b6e6decc484d35c7717d
Eko Januardy Manurung <eko.j.manurung@gdn-commerce.com>
2022-06-07 14:16:06 +0700

    add binary tree problem : find level of tree element

--- src/main/java/ds/BinaryTree.java
+++ src/main/java/ds/BinaryTree.java
@@ -7,4 +7,16 @@ public class BinaryTree {
 
     return 1 + Math.max(countLevel(root.getLeft()), countLevel(root.getRight()));
   }
+
+  public int findLevelOf(Node root, int target, int level) {
+    if (root == null) {
+      return -1;
+    }
+
+    if (root.getData() == target)
+      return level;
+
+    return Math.max(findLevelOf(root.getRight(), target, level + 1),
+        findLevelOf(root.getLeft(), target, level + 1));
+  }
 }
--- src/test/java/ds/BinaryTreeTest.java
+++ src/test/java/ds/BinaryTreeTest.java
@@ -54,4 +54,33 @@ public class BinaryTreeTest {
 
     Assert.assertEquals(4, bt.countLevel(root));
   }
+
+  @Test
+  public void findLevelOf_EmptyTree() {
+    BinaryTree bt = new BinaryTree();
+
+    Assert.assertEquals(-1, bt.findLevelOf(null, 2, 0));
+  }
+
+  @Test
+  public void findLevelOf_OnlyRootTree() {
+    BinaryTree bt = new BinaryTree();
+    Node root = new Node(3);
+
+    Assert.assertEquals(0, bt.findLevelOf(root, 3, 0));
+  }
+
+  @Test
+  public void findLevelOf_TreeComplete() {
+    BinaryTree bt = new BinaryTree();
+    Node root = new Node(1);
+    root.setLeft(new Node(2));
+    root.getLeft().setLeft(new Node(4));
+    root.getLeft().setRight(new Node(5));
+    root.getLeft().getLeft().setLeft(new Node(6));
+    root.getLeft().getLeft().setRight(new Node(7));
+    root.setRight(new Node(3));
+
+    Assert.assertEquals(1, bt.findLevelOf(root, 3, 0));
+  }
 }
