10b900a28705b334bf01e64251d0996e4ab21ab1
Eko Manurung <kukutukkaw@gmail.com>
2023-02-07 14:25:31 +0700

    finish logic

--- src/main/java/paypay/CountWords.java
+++ src/main/java/paypay/CountWords.java
@@ -1,25 +1,70 @@
 package paypay;
 
 public class CountWords {
+  package paypay;
+
+public class CountWords {
   public static void main(String[] args) {
-//    System.out.println("hit", "cog", new String[]{"hot", "dot", "dog"});
-  }
 
-  static int solution(String begin, String end, String[] wordList) {
-    int result = 0;
+        List<String> dict = new ArrayList<String>() {{
+            add("ted");
+            add("tex");
+            add("red");
+            add("tax");
+            add("tad");
+            add("den");
+            add("rex");
+            add("pee");
+        }};
 
-    for (int i = 0; i < wordList.length; i++) {
-      int count = 0;
-      for (int j = 0; j < begin.length(); j++) {
-        if (begin.charAt(j) == wordList[i].charAt(j)) {
-          count++;
-        }
-        if (count == 1) {
-          result++;
-          begin = wordList[i];
-        }
-      }
+        List<String> dict2 = new ArrayList<String>() {{
+            add("a");
+            add("b");
+            add("c");
+        }};
+        System.out.println(ladderLength("red", "tax", dict));
+//        System.out.println(ladderLength("a", "c", dict2));
     }
-    return result;
-  }
+
+    static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+
+        Map<String, List<String>> dictionary = new HashMap<>();
+        wordList.add(beginWord);
+        for (String word : wordList) {
+            for (int i = 0; i < word.length(); i++) {
+                String pattern = word.substring(0, i) + "*" + word.substring(i + 1);
+                dictionary.computeIfAbsent(pattern, v -> new ArrayList<>()).add(word);
+            }
+        }
+
+        Queue<String> q = new ArrayDeque<>();
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+        q.add(beginWord);
+        int result = 1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String word = q.remove();
+                if (word.equals(endWord)) {
+                    return result;
+                }
+                for (int j = 0; j < word.length(); j++) {
+                    String pattern = word.substring(0, j) + "*" + word.substring(j + 1);
+                    for (String neiword : dictionary.getOrDefault(pattern, new ArrayList<>())) {
+                        if (!visited.contains(neiword)) {
+                            visited.add(neiword);
+                            q.add(neiword);
+                        }
+                    }
+                }
+            }
+            result += 1;
+        }
+        return 0;
+    }
 }
